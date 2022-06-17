f46e02afc20728ab400b13bd8a0273ea83fcd501
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-06-17 23:23:25 +0300

    17-06-22(23-33)

--- task003/script.js
+++ task003/script.js
@@ -1,20 +1,20 @@
-const calculate = (sum) => {
-if (quantity > 10) {
-  sum = sum - (sum*0.03); 
-} 
-if (sum > 30000) {
-  sum = sum - ((sum-30000)*0.15); 
-} 
-if (promoCode === 'METHED') {
-  sum = sum - sum*0.1;
-} else if (promoCode === 'G3H2Z1' && sum > 2000) {
-  sum = sum - 500;
-} 
-return sum;
+const calculate = () => {
+  let sum = +prompt('Общая сумма товаров в корзине? ', '');
+  let quantity = +prompt('Общее количество товаров в корзине? ', '');
+  let promoCode = prompt('Введите промокод: ', '');
+  if (quantity > 10 && sum < 30000) {
+    sum = sum - (sum * 0.03);
+  } else if (sum > 30000 && quantity > 10) {
+    sum = (sum - (sum * 0.03)) - (((sum - (sum * 0.03)) - 30000) * 0.15);
+  } else if (sum > 30000 && quantity < 10) {
+    sum = sum - ((sum - 30000) * 0.15);
+  }
+  if (promoCode === 'METHED') {
+    sum = sum - sum * 0.1;
+  } else if (promoCode === 'G3H2Z1' && sum > 2000) {
+    sum = sum - 500;
+  }
+  return sum;
 };
 
-const sum = +prompt('Общая сумма товаров в корзине? ', '');
-const quantity = +prompt('Общее количество товаров в корзине? ', '');
-const promoCode = prompt('Введите промокод: ', '');
-
-alert(calculate(sum));
+alert(calculate());
